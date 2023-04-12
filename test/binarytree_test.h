8178b7112f2b36216e96980b3a35dfdb9358b824
atumr <1028668256@qq.com>
2023-04-12 21:55:17 +0800

    add tests

---
+++ test/binarytree_test.h
@@ -0,0 +1,13 @@
+#ifndef ALGO_ATUMR_BINARYTREE_TEST_H
+#define ALGO_ATUMR_BINARYTREE_TEST_H
+
+#include <gtest/gtest.h>
+
+#include "../include/binarytree.h"
+
+using namespace atumr;
+
+TEST(BinarytreeTest, CreateTest){
+}
+
+#endif
---
+++ test/queue_test.h
@@ -0,0 +1,13 @@
+#ifndef ALGO_ATUMR_QUEUE_TEST_H
+#define ALGO_ATUMR_QUEUE_TEST_H
+
+#include <gtest/gtest.h>
+
+#include "../include/queue.h"
+
+using namespace atumr;
+
+TEST(QueueTest, CreateTest){
+}
+
+#endif
