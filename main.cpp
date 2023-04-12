133366962440beba810b5f20d1731fb897a1bc29
atumr <1028668256@qq.com>
2023-04-12 22:17:39 +0800

    modify #include<*.h>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
 
 #include "test/generic_test.h"
-#include "test/stack_test.h"
+#include "test/insertionsort_test.h"
 #include "test/mergesort_test.h"
 #include "test/quicksort_test.h"
-#include "test/insertionsort_test.h"
+#include "test/stack_test.h"
 
 int main(int argc,char **argv){
   testing::InitGoogleTest(&argc,argv);
